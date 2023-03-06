31e067984b4a8b9e0264510ea0995cc3cc83d24c
thiago9800 <57380109+thiago9800@users.noreply.github.com>
2023-03-06 13:46:23 -0300

    sequência de Fibonacci

---
+++ sequência de Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Informe um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int anterior = 0, atual = 1, proximo = 1;
+        bool pertence = false;
+
+        while (proximo <= numero)
+        {
+            if (proximo == numero)
+            {
+                pertence = true;
+            }
+
+            proximo = anterior + atual;
+            anterior = atual;
+            atual = proximo;
+        }
+
+        if (pertence)
+        {
+            Console.WriteLine("{0} pertence à sequência de Fibonacci.", numero);
+        }
+        else
+        {
+            Console.WriteLine("{0} não pertence à sequência de Fibonacci.", numero);
+        }
+    }
+}
