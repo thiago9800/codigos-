b3a01d6ba66717beec502271b8539c04fa78db74
thiago9800 <57380109+thiago9800@users.noreply.github.com>
2023-03-06 13:48:11 -0300

    inverter os caracteres de um string

---
+++ inverter os caracteres de um string/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Pede ao usuário para digitar uma string
+        Console.WriteLine("Digite uma string:");
+        string entrada = Console.ReadLine();
+
+        // Converte a string para um array de caracteres
+        char[] caracteres = entrada.ToCharArray();
+
+        // Inverte a ordem dos caracteres no array
+        int i = 0;
+        int j = caracteres.Length - 1;
+
+        while (i < j)
+        {
+            char temp = caracteres[i];
+            caracteres[i] = caracteres[j];
+            caracteres[j] = temp;
+            i++;
+            j--;
+        }
+
+        // Converte o array de caracteres de volta para uma string
+        string saida = new string(caracteres);
+
+        // Imprime a string invertida
+        Console.WriteLine("A string invertida é: {0}", saida);
+    }
+}
